f48361119226132783d6ab427f1a562441ee9d5e
davefreekunn <defri.kurniawan@dsn.dinus.ac.id>
2025-06-26 15:01:33 +0700

    CL_01

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -225,9 +225,9 @@
 
     <div class="heroe">
 
-        <h1>Welcome to CodeIgniter <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
+        <h1>Haloo <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
 
-        <h2>The small framework with powerful features</h2>
+        <h2>Hallo</h2>
 
     </div>
 
